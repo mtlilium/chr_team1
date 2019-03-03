a7f14c06d44f9bf3d73d13bba6c605aae31de3af
mtlilium <39237030+mtlilium@users.noreply.github.com>
2019-03-03 22:09:51 +0900

    dekita?

--- Hackathon2019_chr1/Assets/Takayama_Scenes/ItemSpawner.cs
+++ Hackathon2019_chr1/Assets/Takayama_Scenes/ItemSpawner.cs
@@ -8,13 +8,43 @@ using System;
 public class ItemSpawner : MonoBehaviour {
 
 	public GameObject coinPrefab;
+	public GameObject redCoinPrefab;
+	public GameObject yuriPrefab;
 
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 20; i++) {
-			Vector3 offset = new Vector3 (10 * i, 0.0f, 0.0f);
-			GameObject obj = Instantiate (coinPrefab, transform.position + offset, transform.rotation);
-			obj.transform.parent = transform;
+		//yuriの生成
+		for (int i = 1; i <= 15; i++) {
+			int tmp = (int)UnityEngine.Random.Range (10, 40);
+			if (tmp >= 25) {
+				Vector3 offset = new Vector3 (UnityEngine.Random.Range (30, 45) * i, 0.0f, 0.0f);
+				RaycastHit2D hit = Physics2D.Raycast (transform.position + offset, -Vector2.up);
+				hit.point = new Vector2 (hit.point.x, hit.point.y + 1);
+				if (hit.collider != null) {
+					GameObject obj = Instantiate (yuriPrefab, hit.point, transform.rotation);
+					obj.transform.parent = transform;
+				}
+			}
+		}
+		//黄色コインの生成
+		for (int i = 1; i <= 30; i++) {
+			Vector3 offset = new Vector3 (UnityEngine.Random.Range (18, 25) * i,0.0f, 0.0f);
+			RaycastHit2D hit = Physics2D.Raycast (transform.position + offset, -Vector2.up);
+			hit.point = new Vector2 (hit.point.x, hit.point.y + UnityEngine.Random.Range (3, 6));
+			if (hit.collider != null) {
+				GameObject obj = Instantiate (coinPrefab, hit.point, transform.rotation);
+				obj.transform.parent = transform;
+			}
+		}
+		//赤色コインの生成
+		for (int i = 1; i <= 15; i++) {
+			Vector3 offset = new Vector3 (UnityEngine.Random.Range (40, 50) * i,0.0f, 0.0f);
+			RaycastHit2D hit = Physics2D.Raycast (transform.position + offset, -Vector2.up);
+			hit.point = new Vector2 (hit.point.x, hit.point.y + UnityEngine.Random.Range (4, 7));
+			if (hit.collider != null) {
+				GameObject obj3 = Instantiate (redCoinPrefab, hit.point, transform.rotation);
+				obj3.transform.parent = transform;
+			}
 		}
 	}
 	
